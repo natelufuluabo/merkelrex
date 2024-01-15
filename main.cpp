4a0ef59396aad1ed618ea51b06a8a05272cc5e08
aircongo <natelufuluabo@yahoo.ca>
2024-01-14 23:55:51 -0500

    created the function that shows options on screen

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+int printOptions(void);
+
 int main(void)
 {
-    
+    printOptions();
+}
+
+int printOptions(void)
+{
+    std::cout << "1: Print help" << std::endl;
+    std::cout << "2: Print exchange stats" << std::endl;
+    std::cout << "3: Place an ask" << std::endl;
+    std::cout << "4: Place a bid" << std::endl;
+    std::cout << "5: Print wallet" << std::endl;
+    std::cout << "6: Continue" << std::endl;
+
+    return 0;
 }
