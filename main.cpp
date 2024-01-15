916cac1d8b72927efc53eabda2132d880f17a788
aircongo <natelufuluabo@yahoo.ca>
2024-01-15 00:03:40 -0500

    created that reads, validates and store user input after being showed the options

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
 int printOptions(void);
+int getUserInput(void);
 
 int main(void)
 {
-    printOptions();
+    int userInput = getUserInput();
+
+    std::cout << "Picked: " << userInput << std::endl;
 }
 
 int printOptions(void)
@@ -15,6 +18,21 @@ int printOptions(void)
     std::cout << "4: Place a bid" << std::endl;
     std::cout << "5: Print wallet" << std::endl;
     std::cout << "6: Continue" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Type in 1-6" << std::endl;
+    std::cout << "" << std::endl;
 
     return 0;
+}
+
+int getUserInput(void)
+{
+    int userOption;
+    do
+    {
+        printOptions();
+        std::cin >> userOption;
+    } while (userOption < 1 || userOption > 6);
+
+    return userOption;
 }
